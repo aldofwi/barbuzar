3694d11b1847d3ae0b8672141d5276b3d5bbe8e3
Aldo FWI <aldo.fwi@gmail.com>
2020-10-01 23:58:29 -0400

    Updating Some for BB Chat

---
+++ src/Chat/Chat.js
@@ -0,0 +1,148 @@
+import "bootstrap/dist/css/bootstrap.css";
+
+import moment from "moment";
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import { useEffect, useState } from "react";
+import SocketIO from "socket.io-client";
+
+const Chat = props => {
+
+    const socket = SocketIO("http://localhost:"+props.port, { transports: ["websocket"] }); // , "polling"
+
+    const [users, setUsers] = useState([]);
+    const [message, setMessage] = useState("");
+    const [messages, setMessages] = useState([]);
+
+    useEffect(() => {
+
+        //socket.on("connect", () => { socket.emit("username", username); });
+
+        socket.on("users", users => { console.log("CHAT --- Users ConnecteD : ", users); setUsers(users); });
+
+        socket.on("messagetxt", message => { setMessages( messages => [...messages, message]) });
+
+        socket.on("connected", user => {
+            socket.emit("sendtxt", ["--- USER IN ---", user.name]);
+            setUsers(users => [...users, user]);
+        });
+
+        socket.on("disconnected", data => {
+
+            setUsers(users => { return users.filter(user => user.id !== data[0]); });
+            socket.emit("sendtxt", ["--- USER OUT ---", data[1]]);
+            console.log("CHAT --- Logged OUT = ", data[1]);
+
+        });
+// eslint-disable-next-line react-hooks/exhaustive-deps
+    }, []);
+
+    const submit = event => {
+        event.preventDefault();
+        console.log("CHAT --- USERS : ", users);
+        socket.emit("sendtxt", [message, props.barbuser]);
+        setMessage("");
+    };
+
+    return(
+
+        <div className="container">
+
+            <div className="row">
+
+                <div>
+                    <h5 className="messages"><strong> Messages </strong></h5>
+
+                    <div className="msg">
+
+                        {messages.slice(0).reverse().map(({ user, date, text }, index) => (
+
+                            <div key={index} className="row small">
+
+                                <div className="col-sm">
+
+                                    {moment(date).format("h:mma")}
+
+                                    <b className="col-"> [{[user]}] </b>
+
+                                    <em className="col-"> {text} </em>
+
+                                </div>
+
+                            </div>
+
+                        ))}
+
+                    </div>
+
+                </div>
+
+            </div>
+
+                <div className="typetext">
+
+                    <form onSubmit={submit} id="form">
+                        <div className="input-group">
+
+                            <input
+                                type="text"
+                                className="col-sm"
+                                aria-label="Default"
+                                onChange={e => setMessage(e.currentTarget.value)}
+                                placeholder="Type your text here."
+                                value={message}
+                                id="text"
+                            />
+
+                            <span className="input-group-btn">
+                                <button id="submit" type="submit" className="btn btn-dark">
+
+                                    Send
+
+                                </button>
+                            </span>
+
+                        </div>
+                    </form>
+
+                </div>
+
+
+
+        </div>
+
+    );
+
+};
+
+// ========================================
+
+ReactDOM.render(<Chat />, document.getElementById("root"));
+
+// ========================================
+
+export default Chat;
+
+/*
+
+            <div className="row">
+                <div className="col-md-12 mt-4 mb-4">
+
+                    <h6>Hello {username}</h6>
+
+                </div>
+            </div>
+
+ <div className="col-md-4">
+
+                    <h6> Users </h6>
+
+                    <ul id="users">
+
+                        { users.map(({ name, id }) => ( <li key={id}> {name} </li> )) }
+
+                    </ul>
+
+                </div>
+ */
