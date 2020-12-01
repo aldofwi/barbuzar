3896847b2b105ea23dd86dd99d0c38a58e04da82
Aldo FWI <aldo.fwi@gmail.com>
2020-12-01 19:47:16 -0400

    Updates

--- precache-manifest.ea3c9679ed678f5a19f9a4f16aaf6790.js
+++ precache-manifest.ea3c9679ed678f5a19f9a4f16aaf6790.js
@@ -1,6 +1,6 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "1606566c9b133ec66016f7ea88ed862c",
+    "revision": "15cf9bb3b1b5f9e1202e8b083febf62b",
     "url": "/barbuzar/index.html"
   },
   {
@@ -8,7 +8,7 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/barbuzar/static/css/2.8aa5a7f8.chunk.css"
   },
   {
-    "revision": "92675bcf7dc0562e0aa0",
+    "revision": "b89910084c325a6be586",
     "url": "/barbuzar/static/css/main.8e9c81fa.chunk.css"
   },
   {
@@ -20,8 +20,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/barbuzar/static/js/2.7ec528e8.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "92675bcf7dc0562e0aa0",
-    "url": "/barbuzar/static/js/main.e69e44c7.chunk.js"
+    "revision": "b89910084c325a6be586",
+    "url": "/barbuzar/static/js/main.a728f780.chunk.js"
   },
   {
     "revision": "946e3bcd56deb20742c3",
