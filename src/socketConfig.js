001258a413c0ca4344beb71effbfbf1535188bd3
Aldo FWI <aldo.fwi@gmail.com>
2020-12-12 21:29:37 -0400

    Deploy HK

--- src/socketConfig.js
+++ src/socketConfig.js
@@ -10,7 +10,7 @@ import socketBB from 'socket.io-client';
 let ENDPOINT = 'http://localhost:5000';
 
 
-if(process.env.NODE_ENV !== 'development') ENDPOINT = 'https://barbuzar.herokuapp.com/';
+if(process.env.NODE_ENV !== 'development') ENDPOINT = '/';
 
 
 console.log('00 - socketConfig --- Process.env.NODE_ENV = ', process.env.NODE_ENV);
